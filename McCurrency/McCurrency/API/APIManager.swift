d4df7be2a5e099042062ee51b78c8e6ec52eb11c
LimJaeHyeon9298 <limjh9298@gmail.com>
2024-06-03 16:45:55 +0900

    파일 추가

---
+++ McCurrency/McCurrency/API/APIManager.swift
@@ -0,0 +1,8 @@
+//
+//  APIManager.swift
+//  McCurrency
+//
+//  Created by 임재현 on 6/3/24.
+//
+
+import Foundation
---
+++ McCurrency/McCurrency/Model/CurrencyModel.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencyModel.swift
+//  McCurrency
+//
+//  Created by 임재현 on 6/3/24.
+//
+
+import Foundation
